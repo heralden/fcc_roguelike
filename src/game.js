76a287d58c5cca51337ce59b833d554ba865cea1
Kevin <regen@anche.no>
2018-01-18 07:12:30 +0100

    Do not transpose conflicting rooms

--- src/game.js
+++ src/game.js
@@ -14,27 +14,49 @@ export function copy2d(arr2d) {
 export function data2d(arr2d) {
   const ylim = arr2d.length;
   const xlim = arr2d[0].length;
-  const squares = ylim + xlim;
+  const squares = ylim * xlim;
   return { ylim, xlim, squares };
 }
 
-export function transposeRoom(room, coord, area) {
-  area = copy2d(area);
+export function boundaryCoords(room, coord) {
+  const roomData = data2d(room);
+  const boundaryRoom = newRoom(
+    roomData.xlim + 2,
+    roomData.ylim + 2
+  );
 
+  let coords = [];
+  boundaryRoom.forEach((row, y) => row.forEach((char, x) => {
+    coords.push({ y: coord.y + y - 1, x: coord.x + x - 1 });
+  }));
+  return coords;
+}
+
+export function transposeRoom(room, coord, area) {
   const areaData = data2d(area);
   const roomData = data2d(room);
+  // Do not overlap area edges or boundaries
   if (coord.y === 0 || coord.x === 0
     || roomData.ylim + coord.y >= areaData.ylim
     || roomData.xlim + coord.x >= areaData.xlim) {
     return area;
   }
 
+  // Do not put room beside other rooms
+  const isConflicting = boundaryCoords(room, coord)
+    .some(c => area[c.y][c.x] === space);
+  if (isConflicting) {
+    return area;
+  }
+
+  // Transpose room
+  newArea = copy2d(area);
   room.forEach((row, y) => row.forEach((char, x) => {
     const Y = coord.y + y;
     const X = coord.x + x;
-    area[Y][X] = char;
+    newArea[Y][X] = char;
   }));
-  return area;
+  return newArea;
 }
 
 export function newRoom(width, height) {
--- src/game.test.js
+++ src/game.test.js
@@ -9,7 +9,7 @@ it('creates a 2d array filled with char', () => {
   ]);
 });
 
-describe('transposition', () => {
+describe('transposeRoom()', () => {
 
   const area = g.newArea(6, 6);
   const room = g.newRoom(4, 4);
@@ -26,16 +26,30 @@ describe('transposition', () => {
     ]);
   });
 
-  it('doesn\'t transpose room when beyond area boundaries', () => {
+  it('does not transpose room when beyond area boundaries', () => {
     const res = g.transposeRoom(room, { y: 3, x: 3 }, area);
     expect(res).toEqual(area);
   });
 
-  it('doesn\'t transpose room overlapping area edges', () => {
+  it('does not transpose room overlapping area edges', () => {
     const resOver = g.transposeRoom(room, { y: 2, x: 2 }, area);
     const resUnder = g.transposeRoom(room, { y: 0, x: 0 }, area);
     expect(resOver).toEqual(area);
     expect(resUnder).toEqual(area);
+  });
+
+  it('does not transpose room beside other rooms', () => {
+    const smallRoom = g.newRoom(2, 2);
+    let res = g.transposeRoom(smallRoom, { y: 1, x: 1 }, area);
+    res = g.transposeRoom(smallRoom, { y: 3, x: 3 }, res);
+    expect(res).toEqual([
+      [ '#', '#', '#', '#', '#', '#' ],
+      [ '#', '.', '.', '#', '#', '#' ],
+      [ '#', '.', '.', '#', '#', '#' ],
+      [ '#', '#', '#', '#', '#', '#' ],
+      [ '#', '#', '#', '#', '#', '#' ],
+      [ '#', '#', '#', '#', '#', '#' ]
+    ]);
   });
 
 });
